ad1248f87e6bf01a2034e977c0958589e5b4b47e
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-02-15 19:47:10 +0700

    ตั้งชื่อหัว

---
+++ Star_Pattern4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()//พีระมิด//
+{
+  int i,j,k;
+  scanf("%d",&k);
+  for(i=1;i<=k;i++)
+    {
+      for(j=k;j>i;j--)
+      {
+        printf(" ");
+      }
+      for(j=1;j<=i*2-1;j++)
+      {
+       printf("*");
+      }
+      printf("\n");
+    }
+}
