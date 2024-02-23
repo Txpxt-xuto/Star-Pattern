d7488afb7367fd4d0c001733f7e71cfd11136ef0
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-02-23 19:58:38 +0700

    Create Star_Pattern6.c

---
+++ Star_Pattern6.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main()//Heart//
+{
+    int i,j,k,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i=i+2)
+    {
+        printf(" ");
+        for(j=n-i;j>1;j=j-2)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        for(j=n-i;j>1;j--)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(i=n;i>=1;i--)
+    {
+        for(j=n-i;j>=0;j--)
+        {   
+            printf(" ");
+        }
+        for(j=i*2-1;j>0;j--)
+        {   
+            printf("*");
+        }
+        printf("\n");
+    }
+
+}
